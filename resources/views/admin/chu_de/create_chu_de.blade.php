8eeae3f232c0584dee64eb0a36d4b4a978e7f4e4
phanx <phanxuanthang20@gmail.com>
2023-05-09 20:31:58 +0700

    Change: tu_moi logic

--- resources/views/admin/chu_de/create_chu_de.blade.php
+++ resources/views/admin/chu_de/create_chu_de.blade.php
@@ -7,16 +7,10 @@
             <div class="form-group">
                 <label for="name">Name</label>
                 <input type="text" class="form-control" name="CHU_DE_NAME" id="CHU_DE_NAME" placeholder="Input name">
-                @error('CHU_DE_NAME')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
             </div>
             <div class="form-group">
                 <label for="IMAGE">image</label>
                 <input type="file" class="form-control" name="file_upload" id="IMAGE" placeholder="Input image">
-                @error('IMAGE')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
             </div>
             <div class="form-group">
                 <label for="">Category</label>
@@ -26,9 +20,6 @@
                         <option value="{{$category->id}}">{{$category->category_name}}</option>
                     @endforeach
                 </select>
-                @error('CATEGORY')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
             </div>
             <input type="submit" value="Submit">
         </div>
